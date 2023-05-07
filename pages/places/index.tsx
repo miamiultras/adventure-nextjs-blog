1cc904f5de1f2d09280cf9246ab53adc573e5421
eeeyyy <zurawinaiserow@gmail.com>
2023-05-07 11:12:15 +0200

    refactor places

--- pages/places/index.tsx
+++ pages/places/index.tsx
@@ -20,12 +20,12 @@ export async function getStaticProps() {
 
 function Places({ places }: PlacesProps) {
   return (
-    <div className="container mx-auto px-4 py-40">
+    <div className="container mx-auto px-4 py-10">
       <div className="flex flex-wrap justify-center items-center bg-white">
         {places?.map((place: Place) => (
           <div
             key={place.slug}
-            className="m-4 w-64 h-64 relative overflow-hidden"
+            className="m-4 w-64 h-64 relative overflow-hidden rounded-xl"
           >
             <Link href={`/places/${place.slug}`}>
               <Image
@@ -34,7 +34,7 @@ function Places({ places }: PlacesProps) {
                 fill
                 className="hover:opacity-75 transition ease-in-out duration-300 object-cover"
               />
-              <div className="w-full h-full flex items-center justify-center hover:opacity-75 transition ease-in-out duration-300">
+              <div className="w-full h-full flex items-center justify-center">
                 <h2 className="z-10 text-2xl font-bold text-white py-4 px-6">
                   {place.title}
                 </h2>
