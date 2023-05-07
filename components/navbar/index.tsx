0a2cd39fe741ddb1c8ec6552180ef51892ef42e5
eeeyyy <zurawinaiserow@gmail.com>
2023-05-07 10:40:05 +0200

    add logo

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <section className="relative">
-      <nav className="z-10 w-full p-4 flex items-center justify-between text-gray-900">
-        <h1 className="text-3xl font-bold">Logo</h1>
+      <nav className="z-10 w-full p-4 px-8 flex items-center justify-between text-gray-900">
+        <h1 className="text-3xl font-bold">
+          <Image src="logo.svg" alt="logo" width={50} height={50} />
+        </h1>
         <ul className="flex">
           <li className="ml-4">
             <Link href="/" className="text-lg uppercase">
