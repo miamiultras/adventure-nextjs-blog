e92be0727e7926285dd9b1e8f0d4ce3e8c0f581c
eeeyyy <zurawinaiserow@gmail.com>
2023-05-14 09:05:30 +0200

    refactor place page, connect posts to place

--- pages/places/[id].tsx
+++ pages/places/[id].tsx
@@ -14,7 +14,10 @@ const prisma = new PrismaClient();
 
 export async function getStaticProps({ params }: GetStaticPropsContext) {
   const place = await prisma.place.findUnique({
-    where: { slug: params?.id as string },
+    where: { slug: String(params?.id) },
+    include: {
+      posts: true,
+    },
   });
 
   return {
@@ -83,13 +86,8 @@ export default function Place({ place }: PlaceProps) {
         </div>
       </section>
       <section className="bg-white py-4 px-4 sm:px-6 lg:px-8 mb-20">
-        <div className="max-w-3xl mx-auto text-center">
-          <h2 className="text-2xl leading-9 font-medium text-gray-900 sm:text-4xl sm:leading-10 uppercase">
-            Our guides
-          </h2>
-        </div>
         <div className="mt-12 grid gap-8 md:grid-cols-2 xl:grid-cols-3 mx-10">
-          {guides.map((place) => (
+          {place.posts?.map((place) => (
             <div
               key={place.title}
               className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg hover:opacity-90 transition ease-in-out duration-300"
@@ -112,7 +110,7 @@ export default function Place({ place }: PlaceProps) {
                   <h3 className="text-lg leading-6 font-medium text-gray-900 mb-2">
                     {place.title}
                   </h3>
-                  <p className="text-gray-700">{place.description}</p>
+                  <p className="text-gray-700">{place.excerpt}</p>
                 </div>
               </Link>
             </div>
@@ -122,49 +120,3 @@ export default function Place({ place }: PlaceProps) {
     </>
   );
 }
-
-// mock data, TODO insert to db after switching to postgresql or something that supports scalar arrays
-const guides = [
-  {
-    slug: "guide-1",
-    title: "City of Angels",
-    description:
-      "Los Angeles is a sprawling metropolis in Southern California known for its entertainment industry, beaches, and year-round sunshine.",
-    image: "/images/ny.jpg",
-  },
-  {
-    slug: "guide-2",
-    title: "Parisian Romance",
-    description:
-      "Paris is the capital of France and is known for its art, fashion, and cuisine. It's a city that inspires romance and adventure.",
-    image: "/images/lapaz.jpg",
-  },
-  {
-    slug: "guide-3",
-    title: "Tropical Oasis",
-    description:
-      "Hawaii is a tropical paradise known for its beautiful beaches, lush rainforests, and stunning waterfalls. It's a place where you can escape from the hustle and bustle of everyday life.",
-    image: "/images/sag.jpg",
-  },
-  {
-    slug: "guide-4",
-    title: "Urban Jungle",
-    description:
-      "New York City is a vibrant metropolis that never sleeps. It's known for its skyscrapers, world-class museums, and diverse culture.",
-    image: "/images/ee.jpg",
-  },
-  {
-    slug: "guide-5",
-    title: "Land of the Rising Sun",
-    description:
-      "Japan is a country that blends ancient traditions with modern technology. It's known for its cherry blossoms, hot springs, and delicious cuisine.",
-    image: "/images/holly.jpg",
-  },
-  {
-    slug: "guide-6",
-    title: "The Emerald City",
-    description:
-      "Seattle is a city in the Pacific Northwest known for its coffee, music, and tech industry. It's also surrounded by stunning natural scenery, including mountains and forests.",
-    image: "/images/background.jpg",
-  },
-];
