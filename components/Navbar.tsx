0737983e58c7edc0f7b6031fd802cb0c7830a59f
eeeyyy <zurawinaiserow@gmail.com>
2023-06-13 13:17:54 +0200

    fix logo on dynamic pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="z-10 w-full p-4 px-8 flex items-center justify-between text-gray-900">
       <Link href="/">
-        <Image src="logo.svg" alt="logo" width={50} height={50} />
+        <Image src="/logo.svg" alt="logo" width={50} height={50} />
       </Link>
       <ul className="flex">
         <li className="ml-6">
